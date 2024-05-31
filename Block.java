9d399e152eba4070021840b33a5e18f528eae34d
Corey Dai <73034147+CDplayerxOne@users.noreply.github.com>
2024-05-31 15:58:20 -0400

    2024-05-31 after ics class

---
+++ Block.java
@@ -0,0 +1,97 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class Block extends Rectangle {
+
+  private int type;
+  private static int status;
+  private boolean xMoveLeft = false;
+  private boolean xMoveRight = false;
+  private int[][] gridPosition = new int[5][2];
+  public final int yVelocity = 5;
+  public final int xMovement = 5;
+  public static final int BLOCKLENGTH = 20; // length of block
+
+  public int[][] getGridPostion() {
+    return gridPosition;
+  }
+
+  public Block(int x, int y, int type) {
+    super(x, y, BLOCKLENGTH, BLOCKLENGTH);
+    this.type = type;
+  }
+
+  public void keyPressed(KeyEvent e) {
+    // CW block roatation
+    if (e.getKeyCode() == KeyEvent.VK_UP) {
+      // implement in rotate
+      rotate();
+      move();
+    }
+
+    // accelerate downwards
+    if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+      move();
+    }
+
+    // move the block the left
+    if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+      move();
+    }
+
+    // move the block to the right
+    if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+      move();
+    }
+
+    // hold the block and change status
+    if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+
+    }
+  }
+
+  public void keyReleased(KeyEvent e) {
+    if (e.getKeyChar() == 'd') {
+      move();
+    }
+
+    if (e.getKeyChar() == 'a') {
+      move();
+    }
+
+    if (e.getKeyChar() == 'w') {
+      move();
+    }
+
+    if (e.getKeyChar() == 's') {
+      move();
+    }
+  }
+
+  public void move() {
+    y = y + yVelocity;
+  }
+
+  public void rotate() {
+
+  }
+
+  public int getType() {
+    return status;
+  }
+
+  public int getVelocity() {
+    return yVelocity;
+  }
+
+  public void setStatus(int s) {
+    this.status = s;
+  }
+
+  public void draw(Graphics g) {
+    g.setColor(Color.black);
+    g.fillRect(x, y, BLOCKLENGTH, BLOCKLENGTH);
+
+  }
+
+}
--- GamePanel.java
+++ GamePanel.java
@@ -1,116 +1,76 @@
-
-/* GamePanel class acts as the main "game loop" - continuously runs the game and calls whatever needs to be called
-
-Child of JPanel because JPanel contains methods for drawing to the screen
-
-Implements KeyListener interface to listen for keyboard input
-
-Implements Runnable interface to use "threading" - let the game do two things at once
-
-*/
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+
 import javax.swing.*;
+import javax.imageio.*;
 
 public class GamePanel extends JPanel implements Runnable, KeyListener {
 
-  // dimensions of window
-  public static final int GAME_WIDTH = 250;
-  public static final int GAME_HEIGHT = 250;
+  public static final int GAME_WIDTH = 1024;
+  public static final int GAME_HEIGHT = 768;
 
   public Thread gameThread;
   public Image image;
+  public Image background;
   public Graphics graphics;
-  public PlayerBall ball;
+  // public PlayerBall ball;
 
   public GamePanel() {
-    ball = new PlayerBall(GAME_WIDTH / 2, GAME_HEIGHT / 2); // create a player controlled ball, set start location to
-                                                            // middle of screen
-    this.setFocusable(true); // make everything in this class appear on the screen
-    this.addKeyListener(this); // start listening for keyboard input
+    // ball = new PlayerBall(GAME_WIDTH / 2, GAME_HEIGHT / 2);
+    this.setFocusable(true);
+    this.addKeyListener(this);
 
-    // add the MousePressed method from the MouseAdapter - by doing this we can
-    // listen for mouse input. We do this differently from the KeyListener because
-    // MouseAdapter has SEVEN mandatory methods - we only need one of them, and we
-    // don't want to make 6 empty methods
-    addMouseListener(new MouseAdapter() {
-      public void mousePressed(MouseEvent e) {
-        ball.mousePressed(e);
-      }
-    });
+    // addMouseListener(new MouseAdapter() {
+    // public void mousePressed(MouseEvent e) {
+    // ball.mousePressed(e);
+    // }
+    // });
     this.setPreferredSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
 
-    // make this class run at the same time as other classes (without this each
-    // class would "pause" while another class runs). By using threading we can
-    // remove lag, and also allows us to do features like display timers in real
-    // time!
     gameThread = new Thread(this);
     gameThread.start();
+
+    try {
+      background = ImageIO.read(new File("Images/background.png"));
+    } catch (Exception e) {
+    }
   }
 
-  // paint is a method in java.awt library that we are overriding. It is a special
-  // method - it is called automatically in the background in order to update what
-  // appears in the window. You NEVER call paint() yourself
   public void paint(Graphics g) {
-    // we are using "double buffering here" - if we draw images directly onto the
-    // screen, it takes time and the human eye can actually notice flashes of lag as
-    // each pixel on the screen is drawn one at a time. Instead, we are going to
-    // draw images OFF the screen, then simply move the image on screen as needed.
-    image = createImage(GAME_WIDTH, GAME_HEIGHT); // draw off screen
+    image = createImage(GAME_WIDTH, GAME_HEIGHT);
     graphics = image.getGraphics();
-    draw(graphics);// update the positions of everything on the screen
-    g.drawImage(image, 0, 0, this); // move the image on the screen
+    draw(graphics);
+    g.drawImage(image, 0, 0, this);
 
   }
 
-  // call the draw methods in each class to update positions as things move
   public void draw(Graphics g) {
-    ball.draw(g);
+    g.drawImage(background, 0, 0, null);
+    g.setColor(Color.RED);
+    g.fillRect(90, 20, 400, 750);
+    // ball.draw(g);
   }
 
-  // call the move methods in other classes to update positions
-  // this method is constantly called from run(). By doing this, movements appear
-  // fluid and natural. If we take this out the movements appear sluggish and
-  // laggy
   public void move() {
-    ball.move();
+    // ball.move();
   }
 
-  // handles all collision detection and responds accordingly
   public void checkCollision() {
-
-    // if(ball.y<= 0){
-    // ball.y = 0;
-    // }
-    // if(ball.y >= GAME_HEIGHT - PlayerBall.BALL_DIAMETER){
-    // ball.y = GAME_HEIGHT-PlayerBall.BALL_DIAMETER;
-    // }
-    // if(ball.x <= 0){
-    // ball.x = 0;
-    // }
-    // if(ball.x + PlayerBall.BALL_DIAMETER >= GAME_WIDTH){
-    // ball.x = GAME_WIDTH-PlayerBall.BALL_DIAMETER;
-    // }
   }
 
-  // run() method is what makes the game continue running without end. It calls
-  // other methods to move objects, check for collision, and update the screen
   public void run() {
-    // the CPU runs our game code too quickly - we need to slow it down! The
-    // following lines of code "force" the computer to get stuck in a loop for short
-    // intervals between calling other methods to update the screen.
     long lastTime = System.nanoTime();
     double amountOfTicks = 60;
     double ns = 1000000000 / amountOfTicks;
     double delta = 0;
     long now;
 
-    while (true) { // this is the infinite game loop
+    while (true) {
       now = System.nanoTime();
       delta = delta + (now - lastTime) / ns;
       lastTime = now;
 
-      // only move objects around and update screen if enough time has passed
       if (delta >= 1) {
         move();
         checkCollision();
@@ -120,20 +80,14 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
     }
   }
 
-  // if a key is pressed, we'll send it over to the PlayerBall class for
-  // processing
   public void keyPressed(KeyEvent e) {
-    ball.keyPressed(e);
+    // ball.keyPressed(e);
   }
 
-  // if a key is released, we'll send it over to the PlayerBall class for
-  // processing
   public void keyReleased(KeyEvent e) {
-    ball.keyReleased(e);
+    // ball.keyReleased(e);
   }
 
-  // left empty because we don't need it; must be here because it is required to
-  // be overridded by the KeyListener interface
   public void keyTyped(KeyEvent e) {
 
   }
